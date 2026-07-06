0e765aea5c2c3172953d666b46839259202da7e6
Ethan Ng <ethan@turbocharged.ca>
2026-07-05 23:58:44 -0600

    Improved settings, moved common <head> code to _includes/head.html

--- assets/js/theme_toggle.js
+++ assets/js/theme_toggle.js
@@ -1,38 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const root = document.documentElement;
-  const toggleButton = document.getElementById("theme-toggle");
-  const buttonLabel = document.getElementById("theme-toggle-label");
+const root = document.documentElement;
+const toggleButton = document.getElementById("theme-toggle");
+const buttonLabel = document.getElementById("theme-toggle-label");
 
-  function updateButton(theme) {
-    const sunIcon = document.getElementById("sun-icon");
-    const moonIcon = document.getElementById("moon-icon");
-    if (theme == "light") {
-      sunIcon.style.display = "none";
-      moonIcon.style.display = "block";
-      buttonLabel.textContent = "Light";
-    } else {
-      sunIcon.style.display = "block";
-      moonIcon.style.display = "none";
-      buttonLabel.textContent = "Dark";
-    }
+function updateButton(theme) {
+  const sunIcon = document.getElementById("sun-icon");
+  const moonIcon = document.getElementById("moon-icon");
+  if (theme == "light") {
+    sunIcon.style.display = "none";
+    moonIcon.style.display = "block";
+    buttonLabel.textContent = "Light";
+  } else {
+    sunIcon.style.display = "block";
+    moonIcon.style.display = "none";
+    buttonLabel.textContent = "Dark";
   }
+}
 
-  // Load from storage when DOM loads
-  let currentTheme = localStorage.getItem("theme");
-  if (currentTheme) {
-    root.setAttribute("data-theme", currentTheme);
-  }
-  updateButton(currentTheme);
+// Load from storage when DOM loads
+let currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  root.setAttribute("data-theme", currentTheme);
+}
+updateButton(currentTheme);
 
-  // Theme button event listener
-  toggleButton.addEventListener("click", () => {
-    currentTheme =
-      root.getAttribute("data-theme") ||
-      (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+// Theme button event listener
+toggleButton.addEventListener("click", () => {
+  currentTheme =
+    root.getAttribute("data-theme") ||
+    (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 
-    const newTheme = currentTheme === "dark" ? "light" : "dark";
-    updateButton(newTheme);
-    localStorage.setItem("theme", newTheme);
-    root.setAttribute("data-theme", newTheme);
-  });
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+  updateButton(newTheme);
+  localStorage.setItem("theme", newTheme);
+  root.setAttribute("data-theme", newTheme);
 });
