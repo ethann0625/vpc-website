636eedae1c661bd57e93c949c67825734b81f031
Ethan Ng <74840292+ethann0625@users.noreply.github.com>
2026-07-05 23:58:44 -0600

    Improved settings, moved common <head> code to _includes/head.html

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,13 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const settingsButton = document.getElementById("settings-menu-button");
-  const settingsContainer = document.getElementById("settings-container");
+const settingsButton = document.getElementById("settings-menu-button");
+const settingsContainer = document.getElementById("settings-container");
+const test = document.getElementById("test");
 
-  settingsButton.addEventListener("click", () => {
-    if (settingsContainer.style.display) {
-      settingsContainer.style.display =
-        settingsContainer.style.display == "none" ? "block" : "none";
-    } else {
-      settingsContainer.style.display = "block";
-    }
-  });
+settingsButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  settingsContainer.classList.toggle("hidden");
+});
+
+document.addEventListener("click", (event) => {
+  const inMenu = settingsContainer.contains(event.target);
+  const inButton = settingsButton.contains(event.target);
+
+  if (!inMenu && !inButton) {
+    settingsContainer.classList.add("hidden");
+  }
 });
